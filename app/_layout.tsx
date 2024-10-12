e2e0303f4f842df9912a99e04cbca909b9e846eb
mallworth <maxallworthmiles@gmail.com>
2024-10-12 13:17:57 -0700

    IT WORKS

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from "./components/HomeScreen";
+import LoginSignup from './components/LoginSignup';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,8 +10,8 @@ export default function RootLayout() {
   return (
           <Stack.Navigator>
               <Stack.Screen
-                  name="components/HomeScreen"
-                  component={HomeScreen}
+                  name="components/LoginSignup"
+                  component={LoginSignup}
               />
           </Stack.Navigator>
   );
